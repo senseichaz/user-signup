a19c5a4363b297dfe7535782ca3bcecccf4a5d6e
tjarmane <charliehatch@gmail.com>
2017-04-03 18:58:29 -0500

    2 error messages working

--- main.py
+++ main.py
@@ -15,24 +15,62 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 
-html = """
-<form>
-<h2> Signup </h2>
-Username <input type="text" name="username">
-Password <input type="text" name="passwd">
-Verify Password <input type="text" name="passwd_verify">
-Email (optional) <input type="text" name="email">
+class Index(webapp2.RequestHandler):
+    """
+    Handles requests coming into "/" root
+    """
 
-<button>Submit</button>
-</form>
-
-"""
-
-class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write(html)
+        html = """
+        <form action="/welcome" method="post">
+        <h2> Signup </h2>
+        Username <input type="text" name="username"> <br>
+        Password <input type="text" name="password"> <br>
+        Verify Password <input type="text" name="verify"> <br>
+        Email (optional) <input type="text" name="email"> <br>
+
+        <button>Submit</button>
+        </form>
+
+        """
+
+        error = self.request.get("error")
+        if error:
+            error_esc = cgi.escape(error, quote=True)
+            error_element = "<p class='error'>" + error_esc + "</p>"
+        else:
+            error_element = ""
+
+        self.response.out.write(html + error_element)
+
+class Welcome(webapp2.RequestHandler):
+    """
+    Handles requests coming into "/Welcome" root
+    """
+
+    def post(self):
+        # get username
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        # if the user typed nothing at all, redirect and yell at them
+        if (username.strip() == ""):
+            error = "Give me a little more than nothing here, buddy"
+            self.redirect("/?error=" + cgi.escape(error, quote=True))
+        elif " " in username:
+            error = "No usernames with spaces, doofus"
+            self.redirect("/?error=" + cgi.escape(error, quote=True))
+
+        # write output
+        self.response.out.write("<strong> Welcome, " + username + "  </strong>")
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/welcome', Welcome)
 ], debug=True)
